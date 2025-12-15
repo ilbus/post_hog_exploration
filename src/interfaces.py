a14a653712f19021dcdd9396a48ccc3de1edc2d0
ilbus <pawel.ilba@gmail.clm>
2025-12-15 22:04:12 +0100

    PoC: Adding docker service and core data layer

---
+++ src/interfaces.py
@@ -0,0 +1,6 @@
+from typing import Protocol, Optional
+from src.core.schemas import RawEvent
+
+class MessageQueue(Protocol):
+    def push(self, event: RawEvent) -> None: ...
+    def pop(self) -> Optional[RawEvent]: ...
